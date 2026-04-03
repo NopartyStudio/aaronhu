048bc539bc2cd2141d9de1c86eda1a9fb362a636
Aaron <aaronhustudent@163.com>
2026-04-03 21:19:11 +0800

    Init

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Aaronhu",
+	url: "https://example.com/",
+	language: "en",
+	description: "I am writing about my experiences as a naval navel-gazer.",
+	author: {
+		name: "Aaronhu",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
